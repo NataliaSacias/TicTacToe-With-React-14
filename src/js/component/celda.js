df040a93d6ad132812da733b6fac29f953147f05
NataliaSacias <nsacias@gmail.com>
2021-05-03 17:30:02 +0000

    se puede seleccionar uno y uno

--- src/js/component/celda.js
+++ src/js/component/celda.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./celda.css";
 
-const Celda = () => {
-	return <div className="cell"></div>;
+const Celda = props => {
+	return <div className="cell" onClick={() => {}}></div>;
 };
 
 export default Celda;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Celda from "./celda.js";
+import React, { useState } from "react";
+// import Celda from "./celda.js";
 import "./celda.css";
 
 //include images into your bundle
@@ -7,23 +7,87 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [game, setGame] = useState([
+		" ",
+		" ",
+		" ",
+		" ",
+		" ",
+		" ",
+		" ",
+		" ",
+		" "
+	]);
+
+	const [turnoActual, setTurnoActual] = useState("X");
+
+	const marcar = index => {
+		let newGame = game.map((e, i) => {
+			return index === i ? turnoActual : e;
+		});
+
+		// const cambiarTurno = () => {
+		if (turnoActual === "X") {
+			setTurnoActual("O");
+		} else {
+			setTurnoActual("X");
+		}
+		// };
+		setGame(newGame);
+	};
+
+	// funcion de reset
+
+	const restart = () => {
+		setGame([" ", " ", " ", " ", " ", " ", " ", " ", " "]);
+	};
+
+	// funcion para evaluar si hay ganadores
+	const winner = () => {
+		/*
+        0,1,2 - 3,4,5 - 6,7,8
+        0,3,6 - 1,4,7  - 2,5,8
+        0,4,8 - 2,4,6
+        cuadno llame a la funcion win q sea dentro de useEffect
+        */
+	};
+
 	return (
 		<div className="text-center mt-5">
 			<section>
 				<h1 className="game--title">Tic Tac Toe</h1>
 				<div className="game--container">
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
-					<Celda />
+					{game.map((elemento, index) => {
+						return (
+							<div
+								key={index}
+								className="cell"
+								onClick={() => {
+									marcar(index);
+								}}>
+								{elemento}
+							</div>
+						);
+					})}
+					{/* <div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div>
+					<div className="cell" onClick={() => {}}></div> */}
 				</div>
 				<h2 className="game--status"></h2>
-				<button className="game--restart">Restart Game</button>
+				<button
+					className="game--restart"
+					onClick={() => {
+						restart();
+					}}>
+					Restart Game
+				</button>
 			</section>
 			<p>
 				Made by{" "}

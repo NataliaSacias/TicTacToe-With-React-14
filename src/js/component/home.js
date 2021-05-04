e7c9ff8c04718c14bba06ca47b14768cd2773031
NataliaSacias <nsacias@gmail.com>
2021-05-04 19:10:46 +0000

    probando

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -26,16 +26,19 @@ export function Home() {
 			return index === i ? turnoActual : e;
 		});
 
-		// const cambiarTurno = () => {
 		if (turnoActual === "X") {
 			setTurnoActual("O");
 		} else {
 			setTurnoActual("X");
 		}
-		// };
+
 		setGame(newGame);
+        let win = calculateWinner(game)
+        setWinner(win)
 	};
 
+    
+    const [winner, setWinner] = useState("")
 	// funcion de reset
 
 	const restart = () => {
@@ -43,33 +46,59 @@ export function Home() {
 	};
 
 	// funcion para evaluar si hay ganadores
-	const winner = () => {
-		/*
+	// const winner = () => {
+	/*
         0,1,2 - 3,4,5 - 6,7,8
         0,3,6 - 1,4,7  - 2,5,8
         0,4,8 - 2,4,6
         cuadno llame a la funcion win q sea dentro de useEffect
         */
-	};
+
+	// };
+
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a];
+			}
+		}
+		return null;
+	}
 
 	return (
 		<div className="text-center mt-5">
 			<section>
 				<h1 className="game--title">Tic Tac Toe</h1>
-				<div className="game--container">
-					{game.map((elemento, index) => {
+				<h1 className="game--title">Tic Tac Toe</h1>
+    <h1 className="game--title">El ganador es: {winner}1><h1                					{game.map((elemento, index) => {
 						return (
 							<div
 								key={index}
 								className="cell"
 								onClick={() => {
-									marcar(index);
+									if (game[index] == " ") {
+										marcar(index);
+									}
 								}}>
 								{elemento}
 							</div>
 						);
 					})}
-					
 				</div>
 				<h2 className="game--status"></h2>
 				<button
